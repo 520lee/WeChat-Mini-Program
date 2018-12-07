c5b4d076c38555d6417e0a1336df02bdd233cf39
unknown <unknown@example.com>
2018-12-07 09:34:11 +0800

    Initial Commit

---
+++ pages/first/first.js
@@ -0,0 +1,80 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var posts_content = [{
+        date: "Nov 20 2016",
+        title: "比利·林恩的中场故事",
+        content: "内容内容内容好多内容内容内容好多内容内容内容内容好多内容内容内容好多内容内容内容内容好多内容内容内容好多内容",
+        read: "92",
+        collection: "62"
+      },
+      {
+        date: "Set 10 2018",
+        title: "微信·从入门到弃坑",
+        content: "微信·从入门到弃坑内容内容内容微信·从入门到弃坑好多内容内容内容好多内容内容内容内容好多内容内容内容好多内容内容内容内容好多内容内容内容好多内容",
+        read: "92",
+        collection: "62"
+      }
+    ];
+    // this.setData(posts_content);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
